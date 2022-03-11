9204f8c96c57f7d1d2908cbe2807a47c9eb2457c
Вячеслав Иванов <hilaaba@yandex.ru>
2022-03-11 16:53:53 +0300

    Второй коммит

--- homework.py
+++ homework.py
@@ -11,13 +11,12 @@ import time
 
 load_dotenv()
 
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/dfgdfg/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
@@ -27,25 +26,57 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename='app.log',
+    filemode='a',
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+)
+
 
 def send_message(bot, message):
-    bot.send_message(TELEGRAM_CHAT_ID, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        message = f'Сообщение отправлено в чат: {TELEGRAM_CHAT_ID}'
+        logging.info(message)
+    except Exception as error:
+        message = f'При попытке отправки сообщения произошла ошибка: {error}'
+        logging.error(message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
+    timestamp = 0
     params = {'from_date': timestamp}
-    response = requests.get(
-        ENDPOINT,
-        headers=HEADERS,
-        params=params,
-    ).json()
-    return response
+    try:
+        response = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params,
+        ).json()
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError:
+        status_code = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params,
+        ).status_code
+        message = (
+            f'Сбой в работе программы: Эндпоинт {ENDPOINT} недоступен.'
+            f'Код ответа API: {status_code}'
+        )
+        logging.error(message)
 
 
 def check_response(response):
-    if response.status_code == requests.codes.ok:
-        return response.get('homeworks')[0]
+    try:
+        if response.status_code == requests.codes.ok:
+            return response.get('homeworks')[0]
+        raise requests.HTTPError
+    except requests.HTTPError as error:
+        ...
 
 
 def parse_status(homework):
@@ -56,27 +87,30 @@ def parse_status(homework):
 
 
 def check_tokens():
-    for token in (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
-        try:
-            if token:
-                return True
-        except Exception as err:
-            logging.error(
-                f'Отсутствует обязательная переменная окружения {token}',
-                err,
-            )
-            logging.error('Программа принудительно остановлена.')
-            sys.exit()
+    try:
+        if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+            return True
+        raise ValueError
+    except ValueError:
+        tokens = {
+            'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+            'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+            'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
+        }
+        for name_token, token in tokens.items():
+            if not token:
+                message = (
+                    f'Отсутствует обязательная '
+                    f'переменная окружения: {name_token}.'
+                )
+                logging.critical(message)
+        logging.critical('Программа принудительно остановлена.')
+        sys.exit()
 
 
 def main():
     """Основная логика работы бота."""
 
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename='program.log',
-        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
-    )
     check_tokens()
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
